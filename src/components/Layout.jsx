af795fccee8ac4561ddadf82aec02c59f9d29a1f
liewchooichin <liewchooichin@gmail.com>
2024-10-05 21:27:22 +0800

    Remove unsed imports from Layout.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,20 +1,13 @@
 import { ModeProvider } from "../context/ModeContext";
 import { ProductProvider } from "../context/ProductContext";
 import Product from "./Product";
-//import { Outlet } from "react-router-dom";
-import { SelectedIdProvider } 
-  from "../assignment/CurrentIdContext";
-import { EditItemForm } from "../assignment/EditItemForm";
 
 export function Layout(){
   return (
     <div className="App">
       <ModeProvider>
       <ProductProvider>
-      {/* <SelectedIdProvider> */}
         <Product />
-        {/* <EditItemForm /> */}
-      {/* </SelectedIdProvider> */}
       </ProductProvider>
       </ModeProvider>
     </div>     
